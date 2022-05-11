ad90018393fe370cd0a5b8d71f687d7049d94d07
Greta Skiparė <greta.skipare@bbservice.lt>
2022-05-11 07:25:10 +0300

    Chapter 1

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      About page.
+    </>
+  )
+}
+
+export default AboutPage
